59229e25cb9b211c17d7e70c5dc51d7cdb58d1cc
Capo_Samu <capotostisamuel@gmail.com>
2023-11-13 17:14:35 +0100

    Avanzamento1

---
+++ esercizi-java/esercizioalfa/EsercizioAlfa.java
@@ -0,0 +1,6 @@
+public class EsercizioAlfa {
+    public static void main(String[] args) {
+        System.out.println("ciao da samuel il programmatore");
+        System.out.println("Ciao Marika come stai?");
+    }
+}
