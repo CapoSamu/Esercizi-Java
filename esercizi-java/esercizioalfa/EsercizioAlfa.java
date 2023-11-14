8008083be10acc5d4e01f85890cf9eb0613e9906
Capo_Samu <capotostisamuel@gmail.com>
2023-11-14 11:53:31 +0100

    Esercizio-Java

--- esercizi-java/esercizioalfa/EsercizioAlfa.java
+++ esercizi-java/esercizioalfa/EsercizioAlfa.java
@@ -1,6 +1,19 @@
 public class EsercizioAlfa {
     public static void main(String[] args) {
-        System.out.println("ciao da samuel il programmatore");
-        System.out.println("Ciao Marika come stai?");
+        System.out.print("s:ciao da samuel il programmatore");
+        System.out.println("S:Ciao Marika come stai?");
+        // Anthony joined the chat
+        /*
+        ciao
+        test
+        studio
+        java
+        ciao
+        */
+
+        System.out.print("M:Ciao sono Marika");
+        System.out.println("M: Come stai?");
+        // Giorno n.2 studio delle variabili
+        // x = 8 es 3*x + 5 = 29
     }
 }
